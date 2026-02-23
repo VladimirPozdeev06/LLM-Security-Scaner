ed8b03d7f1d97d729b572de26a445797d0ce72ee
vpooz <vova0306pozdeev@yandex.ru>
2026-02-23 14:44:56 +0300

    feat: classifier -add new metricks in compute metricks -transfer evaluation model notebook from kaggle - create prompt classifier

--- prompts_classifier.py
+++ prompts_classifier.py
@@ -1,28 +1,46 @@
+import numpy as np
 from sklearn.model_selection import train_test_split
 from datasets import Dataset
 import pandas as pd
-from transformers import AutoTokenizer
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 
 
-def split_data(data:pd.DataFrame):
-    data=data.rename(columns={'is_unsafe':'label','prompt':'text'})
-    train_data,test_data=train_test_split(data,
-                                          test_size=0.2,
-                                          stratify=data['from_dataset'],
-                                          random_state=17)
-    train_data,val_data=train_test_split(train_data,
-                                         test_size=0.125,
-                                         stratify=train_data['from_dataset'],
-                                         random_state=17)
+def split_data(data: pd.DataFrame):
+    data = data.rename(columns={'is_unsafe': 'label', 'prompt': 'text'})
+    train_data, test_data = train_test_split(data,
+                                             test_size=0.2,
+                                             stratify=data['from_dataset'],
+                                             random_state=17)
+    train_data, val_data = train_test_split(train_data,
+                                            test_size=0.125,
+                                            stratify=train_data['from_dataset'],
+                                            random_state=17)
     train_dataset = Dataset.from_pandas(train_data[['text', 'label']])
     val_dataset = Dataset.from_pandas(val_data[['text', 'label']])
     test_dataset = Dataset.from_pandas(test_data[['text', 'label']])
-    return train_dataset,val_dataset,test_dataset
+    return train_dataset, val_dataset, test_dataset
 
-def tokenize_function(examples,tokenizer):
+
+def tokenize_function(examples, tokenizer):
     return tokenizer(
         examples['text'],
         truncation=True,
         max_length=512
     )
 
+
+def compute_metrics(pred) -> dict:
+    logits, labels = pred
+    predictions = np.argmax(logits, axis=-1)
+
+    acc = accuracy_score(labels, predictions)
+    f1 = f1_score(labels, predictions, average='macro')
+    recall = recall_score(labels, predictions)
+    precision = precision_score(labels, predictions)
+    return {
+        'accuracy': acc,
+        'f1': f1,
+        'recall': recall,
+
+        'precision': precision
+    }
