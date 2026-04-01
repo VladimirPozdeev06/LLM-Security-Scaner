aab766e62556cf05222c28fbcf3b3879be22b13b
vpooz <vova0306pozdeev@yandex.ru>
2026-04-01 14:34:52 +0300

    -feat: final evaluation model -combine all compute functions in one function

--- evaluation_models.py
+++ evaluation_models.py
@@ -16,21 +16,22 @@ import time
 from prepare_data_for_dpo import extract_response_and_analysis, extract_analysis_fields
 from prompts_classifier import predict_batch
 
-def prepare_data_to_necessary_from_for_evaluation(path:str='final_test_data/test_data_final_all_models.csv')->pd.DataFrame:
+def prepare_data_to_necessary_from_for_evaluation(path:str='final_test_data/test_data_final_all_models.csv',text_column:str='text')->pd.DataFrame:
     data = pd.read_csv('/content/drive/MyDrive/test_data_final_all_models.csv', index_col=0)
+    data = data.reset_index(drop=True)
     data['response_part'], data['analysis_part'] = zip(
-        *data['text'].apply(extract_response_and_analysis)
+        *data[text_column].apply(extract_response_and_analysis)
     )
-    fields = data['text'].apply(extract_analysis_fields)
+    fields = data['analysis_part'].apply(extract_analysis_fields)
     fields_df = pd.DataFrame(fields.tolist(), index=data.index)
     data = data.join(fields_df)
-
-    data = data.drop(columns=['is_unsafe'])
+    if 'is_unsafe' in data.columns:
+        data = data.drop(columns=['is_unsafe'])
     print(data.info())
     return data
 
-def compute_format_metrics(data:pd.DataFrame,responses:pd.DataFrame)->None:
-    format_compliance = 1 - round(len(responses[responses['analysis_part'].isna()]) / len(responses), 3)
+def compute_format_metrics(data:pd.DataFrame)->None:
+    format_compliance = 1 - round(len(data[data['analysis_part'].isna()]) / len(data), 3)
     print(f'Наличие аналитической части:{format_compliance:.1%}')
     data['all_analysis'] = (
             data['response_part'].notna() &
@@ -109,5 +110,36 @@ def compute_semantic_metrics(data:pd.DataFrame)->None:
     print(f"BERTScore Recall:    {R.mean().item():.4f}")
     print(f"BERTScore F1:        {F1.mean().item():.4f}")
 
-def compute_quantitative_metrics():
+def compute_quantitative_metrics(data:pd.DataFrame)->None:
     print(f"Средняя длина ответа: {data['response_part'].str.len().mean()}")
+
+def compute_all_metrics_responses(test_data:pd.DataFrame,path_data:str,
+                                 is_format_metrics:bool=True,
+                                 is_numeric_metrics:bool=True,
+                                 is_safety_metrics:bool=True,
+                                 return_confidence:bool=False,
+                                 is_text_metrics:bool=True,
+                                 is_semantic_metrics:bool=True,
+                                 is_quantitative_metrics:bool=True):
+    responses=prepare_data_to_necessary_from_for_evaluation(path_data,text_column='0')
+    responses = responses.rename(columns={'0': 'response'})
+    data = pd.concat([test_data, responses], axis=1)
+    if is_format_metrics:
+        compute_format_metrics(data)
+    if is_numeric_metrics:
+        compute_numeric_metrics(data)
+    if is_safety_metrics:
+        if return_confidence:
+            return compute_safety_metrics(data,return_confidence)
+        else: compute_safety_metrics(data)
+    if is_text_metrics:
+        compute_text_metrics(data)
+    if is_semantic_metrics:
+        compute_semantic_metrics(data)
+    if is_quantitative_metrics:
+        compute_quantitative_metrics(data)
+
+
+
+
+
